8e279b5d811472bc1f46a0f558e39d92d2386c54
Arindam Chakraborty <arismart99@gmail.com>
2014-08-22 12:40:42 +0530

    - Add images for datatables - table init - table setup

--- app/assets/javascripts/domain/index.js
+++ app/assets/javascripts/domain/index.js
@@ -1,1 +1,15 @@
 //= require plugin_files/jquery.dataTables.min
+
+function DomainList(config) {
+  this.config = config;
+  this._init();
+}
+
+DomainList.prototype._init = function() {
+  var that = this;
+  $('table.main-data').DataTable({
+    deferRender: true,
+    lengthChange: false,
+
+  });
+};
